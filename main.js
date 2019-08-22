798ee0b874cabddfff5dac8b0ee70ff416f575f1
Heather Faerber <heatherdfaerber@gmail.com>
2019-08-22 14:20:33 -0600

    Add functionality to max-range

--- main.js
+++ main.js
@@ -8,10 +8,14 @@
 // var minRangeInput = document.querySelector("#min-range-input").value;
 var minRangeField = document.querySelector("#min-range-input");
 var rangeStart = document.querySelector("#range-start");
+var maxRangeField = document.querySelector("#max-range-input");
+var rangeEnd = document.querySelector("#range-end");
 var submitRangeButton = document.querySelector("#submit-range-button");
 
 submitRangeButton.addEventListener("click", function() {
   var minRangeValue = minRangeField.value;
   rangeStart.innerText = minRangeValue;
+  var maxRangeValue = maxRangeField.value;
+  rangeEnd.innerText = maxRangeValue;
   event.preventDefault();
 });
