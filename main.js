707320cc574e9edb1fe8a7d59b2f6878b3fe4d72
Heather Faerber <heatherdfaerber@gmail.com>
2019-08-24 12:21:42 -0600

    Refactor maxRangValue to match minRangeValue functionality

--- main.js
+++ main.js
@@ -1,25 +1,13 @@
-// set range box functionality for min Range
-
-// minRangeInput could also be broken down into two variables:
-
-// var minRangeField = document.querySelector("#min-range-input");
-// var minRangeValue = minRangeField.value;
-//
-// var minRangeInput = document.querySelector("#min-range-input").value;
-// var minRangeField = document.querySelector("#min-range-input");
 var minRangeValue = 1;
 var rangeStart = document.querySelector("#range-start");
-var maxRangeField = document.querySelector("#max-range-input");
+var maxRangeValue = 100;
 var rangeEnd = document.querySelector("#range-end");
 var submitRangeButton = document.querySelector("#submit-range-button");
 
 submitRangeButton.addEventListener("click", function() {
   minRangeValue = parseInt(document.querySelector("#min-range-input").value, 10);
-  // the minRangeValue var might be an issue bc it will only be able to be called in this function
-  // var intMin = parseInt(minRangeValue, 10);
-  // rangeStart.innerText = intMin;
   rangeStart.innerText = minRangeValue;
-  var maxRangeValue = maxRangeField.value;
+  maxRangeValue = parseInt(document.querySelector("#max-range-input").value, 10);
   rangeEnd.innerText = maxRangeValue;
   event.preventDefault();
 });
