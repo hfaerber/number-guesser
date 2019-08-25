89a9be5aebbc7900644c080fffd2284fab81ff41
sertmer <scottertmer@gmail.com>
2019-08-25 11:29:52 -0600

    Build random number generator function

--- main.js
+++ main.js
@@ -19,6 +19,12 @@ submitRangeButton.addEventListener("click", setRange);
 submitGuessButton.addEventListener("click", displayGameInfo);
 
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max-min)) + min;
+}
+
 function setRange(event){
   console.log(minRangeValue.value);
   console.log(maxRangeValue.value);
