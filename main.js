1bce59c1a9dfff22af30e67ec8a01544d30d641f
Heather Faerber <heatherdfaerber@gmail.com>
2019-08-28 15:26:34 -0600

    Comment out js createelement functionality so we can reroute

--- main.js
+++ main.js
@@ -24,7 +24,6 @@ var winnerCardName = document.querySelector('#winner-card-name');
 var randomNum;
 var highLowOne = document.querySelector('.high-low-one');
 var highLowTwo = document.querySelector('.high-low-two');
-var errorMsg = createErrorMsg()
 
 // EVENT LISTENERS
 updateButton.addEventListener('click', setRange);
@@ -58,6 +57,7 @@ function setRange(event){
   rangeEnd.innerHTML = maxRangeValue.value;
   getRandomInt();
   console.log(randomNum);
+
 };
 
 function displayGameInfo(event){
@@ -136,26 +136,21 @@ function gameHint() {
   }
 };
 
-function errorCheckRange() {
-  if (parseInt(minRangeValue.value) > parseInt(maxRangeValue.value)) {
-    // var errorMsg = createErrorMsg()
-    rangeInputBox.appendChild(errorMsg);
-    }
-  if (parseInt(minRangeValue.value) < parseInt(maxRangeValue.value)) {
-    rangeInputBox.removeChild(errorMsg);
-  }
-}
+// function errorCheckRange() {
+//   if (parseInt(minRangeValue.value) > parseInt(maxRangeValue.value)) {
+      // var errorMsg = createErrorMsg()
+//     rangeInputBox.appendChild(errorMsg);
+//     }
 
-function createErrorMsg() {
-  var errorP = document.createElement("p");
-  errorP.setAttribute('class', 'error-message')
-  errorP.innerText = "Invalid Entry";
-  return errorP;
-}
+//   if (parseInt(minRangeValue.value) < parseInt(maxRangeValue.value) &&
+// rangeInputBox.contains('.error-message')) {
+//       rangeInputBox.removeChild(errorMsg);
+//     }
+// };
 
-// trigger error on update button
-// unhide the error message that was hidden under the ranges
-// function errorCheckGuess() {
-//   if (parseInt(.value)
-// )
+// function createErrorMsg() {
+//   var errorP = document.createElement("p");
+//   errorP.setAttribute('class', 'error-message')
+//   errorP.innerText = "Invalid Entry";
+//   return errorP;
 // }
